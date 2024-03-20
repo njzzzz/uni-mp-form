46b1a163635c87b5b8ead19d4e2e45d0040a1049
nijr@citycloud.com.cn <nijr@citycloud.com.cn>
2024-03-20 18:03:33 +0800

    feat: init

---
+++ src/components/mp-form/MpForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps({
+  schema: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+</script>
+
+<template>
+  <uni-forms />
+</template>
+
+<style scoped lang="scss">
+
+</style>
